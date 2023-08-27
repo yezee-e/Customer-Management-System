8ed0dd8ea432d3083480300364f3175980697ca0
yeji <yejipooh@gmail.com>
2023-08-28 02:27:25 +0900

    create react project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='gray-background'>
+      <h2>let's develop management system!</h2>
+    </div>
+  );
+}
+
+export default App;
