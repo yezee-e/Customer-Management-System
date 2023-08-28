fade410908e1418de9a69611b8cf9f9a4b8e131a
yeji <yejipooh@gmail.com>
2023-08-28 18:42:01 +0900

    apply Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  Paper, //스타일을 주기 위해 외부를 감싸는 역할
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
 import './App.css';
 import Customer from './components/Customer';
+
+const useStyles = makeStyles({
+  //전체적용
+  root: {
+    width: '100%',
+    marginTop: '30px', //탑여백을 3의 가중치 만큼 준다?
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 1080, //가로 테이블이 무조건 이 크기이기 때문에 화면을 줄이면 가로 스크롤바가 생긴다
+  },
+});
 
 const customers = [
   {
@@ -29,21 +51,39 @@ const customers = [
 ];
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className='gray-background'>
-      {customers.map((customer) => (
-        <Customer
-          key={customer.id}
-          id={customer.id}
-          image={customer.image}
-          name={customer.name}
-          birthday={customer.birthday}
-          gender={customer.gender}
-          job={customer.job}
-        />
-      ))}
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((customer) => (
+            <Customer
+              key={customer.id}
+              id={customer.id}
+              image={customer.image}
+              name={customer.name}
+              birthday={customer.birthday}
+              gender={customer.gender}
+              job={customer.job}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
 export default App;
+
+//material-UI
+//설치: npm install @mui/material @emotion/react @emotion/styled
--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { TableRow, TableCell } from '@mui/material';
 import CustomerProfile from './CustomerProfile';
 import CustomerInfo from './CustomerInfo';
 
 function Customer({ id, image, name, birthday, gender, job }) {
   return (
-    <div>
-      <CustomerProfile id={id} image={image} name={name} />
-      <CustomerInfo birthday={birthday} gender={gender} job={job} />
-    </div>
+    <TableRow>
+      <TableCell>{id}</TableCell>
+
+      <TableCell>
+        <img src={image} alt='profile' />
+      </TableCell>
+      <TableCell>{name}</TableCell>
+      <TableCell>{birthday}</TableCell>
+      <TableCell>{gender}</TableCell>
+      <TableCell>{job}</TableCell>
+    </TableRow>
   );
 }
 
